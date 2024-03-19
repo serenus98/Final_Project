89cd224d6c88384d3ec19fe66c853eb316a39df0
Philippe Furlano <phil.furlano@gmail.com>
2024-03-19 15:01:33 +0100

    new fucntions for better data parsing

---
+++ App.py
@@ -0,0 +1,9 @@
+# Import the Tkinter library for GUI development
+import tkinter as tk
+# If you're using Python 2, you would import it as:
+# import Tkinter as tk
+# Create the main application window
+root = tk.Tk()
+# Additional libraries can be imported as needed for your specific project
+# For example, if you plan to work with dropdown menu options, you might import tkinter.ttk
+# import tkinter.ttk as ttk
--- find_homolog.py
+++ find_homolog.py
@@ -13,7 +13,9 @@ working_directory = Path(__file__).absolute().parent
 eggnog4_species_list = "data/eggnog4.species_list.txt"
 eggnog4_species_list_filename_path = working_directory / eggnog4_species_list
 meNOG_members = "data/meNOG.members.tsv"
+me_NOG_test = "data/Test_members_data.txt"
 meNOG_members_filename_path = working_directory / meNOG_members
+me_NOG_test_filename_path = working_directory / me_NOG_test
 
 def readFile(filePath):
     # reading a file and returning a list of lines
@@ -23,6 +25,7 @@ def readFile(filePath):
 species_list = readFile(eggnog4_species_list_filename_path)
 species1 = "Homo sapiens"
 species2 = "Mus musculus"
+species3 = "Pan troglodytes"
 #print(species_list[1:10])
 
 def find_TaxonID (species_name, species_list=species_list):
@@ -37,15 +40,101 @@ def find_TaxonID (species_name, species_list=species_list):
     return species_tax_id
 
 species1_tax_id = find_TaxonID(species1)
-#species2_tax_id = find_TaxonID(species2)
+species2_tax_id = find_TaxonID(species2)
+species3_tax_id = find_TaxonID(species3)
+print(species1_tax_id)
+print(species2_tax_id)
+print(species3_tax_id)
+#members_list = readFile(meNOG_members_filename_path)
 
-members_list = readFile(meNOG_members_filename_path)
-species1_NOG_list = []
+members_list = readFile(me_NOG_test_filename_path)
 
-for element in members_list:
-    if species1_tax_id in element.split("\t")[5]:
-        species1_NOG = element.split("\t")[1]
-        species1_NOG_list.append(species1_NOG)
-#print(members_list[49:51])
-print(species1_NOG_list[:10])
-print(len(species1_NOG_list))
+#def find_NOGs (taxon_ID, members_list = members_list):
+#    NOG_dict = {}
+#
+#    for line in members_list:
+#        Nog = line.split("\t")[1]
+#        TaxonID_ProteinID = line.split("\t")[5]
+#        if taxon_ID in TaxonID_ProteinID:
+#            NOG_dict[Nog] = TaxonID_ProteinID
+#    return NOG_dict
+
+def find_NOGs (taxon_ID, members_list = members_list):
+    NOG_set = []
+
+    for line in members_list:
+            Nog = line.split("\t")[1]
+            TaxonID_ProteinID = line.split("\t")[5]
+            if taxon_ID in TaxonID_ProteinID:
+                NOG_set.append(TaxonID_ProteinID)
+    return NOG_set
+
+def find_NOG_and_proteinID(taxon_ID, members_list = members_list):
+    NOG_dict = {}
+    NOG_dict_result = {}
+    ID_list = []
+    for line in members_list:
+            Nog = line.split("\t")[1]
+            TaxonID_ProteinID = line.split("\t")[5]
+            NOG_dict[Nog] = TaxonID_ProteinID
+            #if taxon_ID in TaxonID_ProteinID:
+            #    NOG_dict.append(TaxonID_ProteinID)
+    for key in NOG_dict:
+        new_protein_list = []
+        NOG_dict[key] = NOG_dict[key].split(",")
+        for protein in NOG_dict[key]:
+            new_protein_list.append(protein.split(".")) 
+        NOG_dict[key] = new_protein_list
+    for key in NOG_dict:
+        for protein in NOG_dict[key]:
+            if protein[0] == taxon_ID:
+                NOG_dict_result[key] =NOG_dict[key]
+    return NOG_dict_result
+
+def NOG_dict_len(taxon_ID):
+    dict = find_NOG_and_proteinID(taxon_ID)
+    return len(dict)
+
+def NOG_list(taxon_ID):
+    NOGs_list = []
+    dict = find_NOG_and_proteinID(taxon_ID)
+    for key in dict:
+        NOGs_list.append(key)
+    return NOGs_list
+
+species1_NOG = find_NOGs(species1_tax_id)
+species2_NOG = find_NOGs(species2_tax_id)
+species3_NOG = find_NOGs(species3_tax_id)
+print("species1_NOGs: ", len(species1_NOG))
+print("species2_NOGs: ", len(species2_NOG))
+print("species3_NOGs: ", len(species3_NOG))
+
+def find_homologs(species1_NOG, species2_NOG):
+    homologs = []
+    for nog1 in species1_NOG:
+        if nog1 in species2_NOG:
+            homologs.append(nog1)
+    return homologs
+
+homologs_mus_homo = find_homologs(species1_NOG, species2_NOG)
+
+def find_nonhomologs(species1_NOG, species2_NOG):
+    nonhomologs = []
+    for nog1 in species1_NOG:
+        if nog1 not in species2_NOG:
+            nonhomologs.append(nog1)
+    return nonhomologs
+
+Hs_Pt_Homologs = find_homologs(species1_NOG, species3_NOG)
+Mm_Pt_Homologs = find_homologs(species2_NOG, species3_NOG)
+nonhomologs = find_nonhomologs(Mm_Pt_Homologs, Hs_Pt_Homologs)
+
+#nonhomologs = find_nonhomologs(species1_NOG, species2_NOG)
+#homologs = find_homologs(species3_NOG, nonhomologs )
+#print("nonhomologs of species 1 and 2:", len(nonhomologs))
+#print("homologs:", len(homologs))
+
+print("nonhomologs of Hs and Mm in Pt: ", len(nonhomologs))
+print(find_NOG_and_proteinID(species1_tax_id))
+print(NOG_dict_len(species1_tax_id))
+print(NOG_list(species1_tax_id))
